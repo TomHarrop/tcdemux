ef3f1d5b8d90f829d7636a59f5e386792bcfeaeb
Thomas Harrop <thomas.harrop@csiro.au>
2023-08-11 09:06:14 +1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+# load README.rst
+def readme():
+    with open('README.rst') as file:
+        return file.read()
+
+
+setup(
+    name='tcdemux',
+    version='0.0.1',
+    description=(
+        'python3 wrapper for demultiplexing '
+        'target capture sequencing results'
+        ),
+    long_description=readme(),
+    url='https://github.com/tomharrop/tcdemux',
+    author='Tom Harrop',
+    author_email='twharrop@gmail.com',
+    license='GPL-3',
+    packages=find_packages(),
+    install_requires=[
+        'biopython>=1.81',
+        'pandas>=2.0.3',
+        'snakemake>=7.31.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tcdemux = tcdemux.__main__:main'
+            ],
+    },
+    scripts={
+        'tcdemux/src/write_barcode_file.py',
+    },
+    package_data={
+        'csdemux': [
+            'Snakefile',
+            'README.rst'
+        ],
+    },
+    zip_safe=False)
