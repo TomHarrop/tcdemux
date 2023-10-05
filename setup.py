680cf5cb24c80508b5207b87995f2621f7ea7704
Thomas Harrop <thomas.harrop@csiro.au>
2023-10-05 17:34:25 +1100

    added cutadapt stat parsing

--- setup.py
+++ setup.py
@@ -7,40 +7,37 @@ from setuptools import find_packages
 
 # load README.rst
 def readme():
-    with open('README.rst') as file:
+    with open("README.rst") as file:
         return file.read()
 
+
 setup(
-    name='tcdemux',
-    version='0.0.15',
+    name="tcdemux",
+    version="0.0.15",
     description=(
-        'python3 wrapper for demultiplexing '
-        'target capture sequencing results'
-        ),
+        "python3 wrapper for demultiplexing " "target capture sequencing results"
+    ),
     long_description=readme(),
-    url='https://github.com/tomharrop/tcdemux',
-    author='Tom Harrop',
-    author_email='twharrop@gmail.com',
-    license='GPL-3',
+    url="https://github.com/tomharrop/tcdemux",
+    author="Tom Harrop",
+    author_email="twharrop@gmail.com",
+    license="GPL-3",
     packages=find_packages(),
     install_requires=[
-        'biopython>=1.81',
-        'cutadapt>=4.4',
-        'pandas>=2.0.3',
-        'snakemake>=7.31.0'
+        "biopython>=1.81",
+        "cutadapt>=4.4",
+        "pandas>=2.0.3",
+        "snakemake>=7.31.0",
     ],
     entry_points={
-        'console_scripts': [
-            'tcdemux = tcdemux.__main__:main'
-            ],
+        "console_scripts": ["tcdemux = tcdemux.__main__:main"],
     },
     scripts={
-        'tcdemux/src/write_barcode_file.py',
+        "tcdemux/src/parse_cutadapt_stats.py",
+        "tcdemux/src/write_barcode_file.py",
     },
     package_data={
-        'tcdemux': [
-            'Snakefile',
-            'README.rst'
-        ],
+        "tcdemux": ["Snakefile", "README.rst"],
     },
-    zip_safe=False)
+    zip_safe=False,
+)
--- tcdemux/src/parse_cutadapt_stats.py
+++ tcdemux/src/parse_cutadapt_stats.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
+import sys
 import json
 import pandas as pd
 
-stats_file = Path("tests/pooled/stats/cutadapt/AG_P8P5C.json")
+# Read JSON input from stdin
+input_json = sys.stdin.read()
+stats_json = json.loads(input_json)
 
-with stats_file.open("r") as f:
-    stats_json = json.loads(f.read())
-
-# create dataframe
+# Create a DataFrame from the JSON data
 cutadapt_stats = pd.json_normalize(stats_json)
 
-# parse the adaptor info
+# Extract adapter statistics for each library
 library_count = {}
 adaptor_stats = cutadapt_stats.iloc[0]["adapters_read1"]
 
 for sample in adaptor_stats:
     library_count[sample["name"]] = sample["total_matches"]
 
-s = pd.Series(library_count, name="reads_assigned")
-s.index.name = "library"
-processed_stats = pd.DataFrame(s)
-processed_stats["total_input"] = cutadapt_stats.iloc[0]["read_counts.input"]
-processed_stats["total_matched"] = cutadapt_stats.iloc[0][
+# Create a Series with library counts and assign it to a DataFrame
+processed_stats = pd.Series(library_count, name="reads_assigned").reset_index()
+processed_stats.rename(columns={"index": "library"}, inplace=True)
+
+# Add additional columns to the DataFrame
+processed_stats["pool_total_input"] = cutadapt_stats.iloc[0]["read_counts.input"]
+processed_stats["pool_total_matched"] = cutadapt_stats.iloc[0][
     "read_counts.read1_with_adapter"
 ]
-
-processed_stats["fraction_of_total"] = processed_stats["reads_assigned"].div(
-    processed_stats["total_input"]
+processed_stats["fraction_of_total"] = (
+    processed_stats["reads_assigned"] / processed_stats["pool_total_input"]
+)
+processed_stats["fraction_of_matched"] = (
+    processed_stats["reads_assigned"] / processed_stats["pool_total_matched"]
 )
 
-processed_stats["fraction_of_matched"] = processed_stats["reads_assigned"].div(
-    processed_stats["total_matched"]
-)
+# Output the processed CSV data to stdout
+processed_stats.to_csv(sys.stdout, index=False)
