3d09256699b2ae42fb83fab4649af1b2ecd27413
Thomas Harrop <thomas.harrop@csiro.au>
2023-10-16 13:49:44 +1100

    have to tell clumpify to use tmp

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name="tcdemux",
-    version="0.0.23",
+    version="0.0.24",
     description=(
         "python3 wrapper for demultiplexing target capture sequencing results"
     ),
