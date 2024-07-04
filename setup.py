db61907da38a575df6d03075652c617300d0c167
Thomas Harrop <thomas.harrop@csiro.au>
2024-07-04 15:25:44 +1000

    add quality trimming to final step

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name="tcdemux",
-    version="0.0.25",
+    version="0.0.26",
     description=(
         "python3 wrapper for demultiplexing target capture sequencing results"
     ),
